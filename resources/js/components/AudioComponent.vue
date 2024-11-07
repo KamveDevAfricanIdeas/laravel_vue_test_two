eb1ee36bc648d9ea753074eac6880216814850e0
Kamvelihle Gwijana <kamvelihle.gwijana@dev.africanideas.co.za>
2024-11-07 15:50:19 +0200

    dynamic button content basic

--- resources/js/components/AudioComponent.vue
+++ resources/js/components/AudioComponent.vue
@@ -1,12 +1,15 @@
 <template>
     <div class="flex flex-col justify-center items-center">
         <img class="w-20" src="https://th.bing.com/th/id/R.5947b89e564be064930ad9fc96d1f11e?rik=OAHX4feNsN4nAQ&riu=http%3a%2f%2fclipart-library.com%2fimages_k%2fheadphones-transparent-png%2fheadphones-transparent-png-16.png&ehk=79lEjJ%2fz48Rj3sRvpCPEimp2Eum9%2bIrgpy%2bFHO4yzE8%3d&risl=&pid=ImgRaw&r=0">
-        <p class="text-info">Content info here</p>
+        <button type="button" class="tool-button" name="default-tool">{{btnLabel}}</button>
     </div>
 </template>
 <script>
     export default{
         name: 'AudioComponent',
+        props:{
+            btnLabel: String
+        },
         mounted(){
         console.log("Audio Component component added.");
     }
--- resources/js/components/AudioToolsSection.vue
+++ resources/js/components/AudioToolsSection.vue
@@ -1,6 +1,9 @@
 <template>
+    <!-- TODO:
+        make each button dynamic and load different labels. {"master track", "slow+reverb", "speedup", "cleanup"}
+    -->
     <div class="content-style gap-3">
-        <audio-component v-for="n in 4" :key="n"/>
+        <audio-component v-for="(name, index) in buttonNames" :key="index" :btnLabel="name"/>
     </div>
 </template>
 <script>
@@ -9,6 +12,11 @@ import AudioComponent from './AudioComponent.vue';
 export default {
     components: {
         AudioComponent
-    }
+    },
+    data() {
+            return {
+                buttonNames: ["Master Track", "Slow+Reverb", "Speedup", "Cleanup"]
+            };
+        }
 };
 </script>
